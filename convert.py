9c6736c3c4f1fa301c435f53295a6a3a78952d99
Zhereng Zhang <zzhang@debian64VM.vmthing.com>
2014-03-17 02:26:12 -0400

    Another bug fix

--- convert.py
+++ convert.py
@@ -5,8 +5,8 @@ from nltk.corpus import wordnet as wn
 def convert(sentence):
     out = []
     for wd in sentence:
-        ll = wn.lemmas(wd['word'])
-        if len(ll)>0:
-            wd['word'] = ll[0].name
+        s = wn.synsets(wd['word'])
+        if len(s)>0:
+            wd['word'] = s[0].lemmas[0].name
         out.append(wd)
     return out
