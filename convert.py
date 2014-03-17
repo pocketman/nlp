54501c97b630d65fb59d4efcd37f1458266c2eb2
Zhereng Zhang <zzhang@debian64VM.vmthing.com>
2014-03-17 02:41:38 -0400

    deep copy

--- convert.py
+++ convert.py
@@ -6,6 +6,7 @@ def convert(sentence):
     out = []
     for wd in sentence:
         s = wn.synsets(wd['word'])
+        o = copy.deepcopy(s)
         if len(s)>0:
             wd['word'] = s[0].lemmas[0].name
         out.append(wd)
